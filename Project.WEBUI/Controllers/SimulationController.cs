8abd9e0e0551feec975d5f1324e70cb884c64107
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-05-29 13:01:24 +0300

    Simulation Controller eklendi

---
+++ Project.WEBUI/Controllers/SimulationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WEBUI.Controllers
+{
+    public class SimulationController : Controller
+    {
+        // GET: Simulation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
